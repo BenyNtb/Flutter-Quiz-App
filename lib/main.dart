afae6546b47c7e7eb94e51c2b73438a7a7c90466
Beny <b.ntambwe97@gmail.com>
2022-07-06 14:59:55 +0200

    Resetting the Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,13 @@ class _MyAppState extends State < MyApp > {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
@@ -80,7 +87,7 @@ class _MyAppState extends State < MyApp > {
           questionIndex: _questionIndex,
           questions: _questions,
         ) :
-        Result(_totalScore),
+        Result(_totalScore, _resetQuiz),
       ),
     );
   }
